b7b4d723fd9e41acda437777cadb97229d6d738c
Jinsu Oh <51368746+Laid41798175@users.noreply.github.com>
2024-04-07 20:37:49 +0900

    edited env name

--- netlify/functions/getItem.js
+++ netlify/functions/getItem.js
@@ -5,13 +5,15 @@ const AWS = require('aws-sdk');
 // AWS SDK 구성
 const dynamodb = new AWS.DynamoDB({
     region: 'ap-northeast-2',
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+    accessKeyId: process.env.ACCESS_KEY_ID,
+    secretAccessKey: process.env.SECRET_ACCESS_KEY
 });
 
 exports.handler = async (event) => {
     // DynamoDB 업데이트 파라미터
     const params = JSON.parse(event.body);
+
+    console.log(params);
 
     try {
         const data = await dynamodb.getItem(params).promise();
